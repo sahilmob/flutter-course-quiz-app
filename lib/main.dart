27d979e2fdaffae03f02770559e443ea9618bb50
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-12-28 19:04:00 +0200

    create app basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Text("Hello")),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("What's your favorite color"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: () {},
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () {},
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
